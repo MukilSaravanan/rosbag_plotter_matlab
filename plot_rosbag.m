c7800196a2d2ee3581761bd8478f42b7a05cde0e
Mukil Saravanan <senjerimalaimukil@gmail.com>
2022-09-12 12:35:11 +0530

    Add comment

--- plot_rosbag.m
+++ plot_rosbag.m
@@ -1,25 +1,33 @@
 function plot_rosbag(full_path)
 
-if ~ exist('full_path','var')
-[file,path]= uigetfile('*.bag');
+% Plots graph from rosbag data
+% Input:
+%   full_path (optional): path to the rosbag
+% Output:
+%   None
+%   Displays graph 
+% Written by Mukil Saravanan.
+
+if ~exist('full_path','var')
+[file,path]= uigetfile('*.bag'); %gui for selecting rosbag
 full_path=fullfile(path,file);
 end
 
 bag=rosbag(full_path);
-display(bag.AvailableTopics(:,1:2));
+display(bag.AvailableTopics(:,1:2)); %display available rostopics contained in the rosbag
 
-choice = input("Enter the topic number  ");
-topic=char(bag.AvailableTopics.Properties.RowNames(choice));
+choice = input("Enter the topic number  "); 
+topic=char(bag.AvailableTopics.Properties.RowNames(choice)); 
 
-bagSelTopic = select(bag,'Topic',topic);
+bagSelTopic = select(bag,'Topic',topic); %select rostopic
 
 topictype=char(bag.AvailableTopics{choice,2});
 pointMsg = rosmessage(topictype);
-showdetails(pointMsg);
+showdetails(pointMsg); %display the rostopic inner datatypes
 
 valid=0;
 
-while ~ valid
+while ~valid %prompt until a valid rostopic is obtained 
     param=input("Enter the parameter  ",'s');
     try
         ts=timeseries(bagSelTopic,param);
@@ -29,18 +37,18 @@ while ~ valid
     end
 end
 
-time=ts.Time-ts.Time(1);
+time=ts.Time-ts.Time(1); %calculate relative time data
 data=ts.Data;
 
 full_topic=strcat(topic,'/',strrep(lower(param),'.','/'));
 y_label_input=input('Enter the y label name,unit (Enclose within codes) ');
-y_label_splitted=regexp(y_label_input,',','split');
+y_label_splitted=regexp(y_label_input,',','split'); %split the obtained input
 
-plot(time,data)
-title(full_topic,'Interpreter','None')
-xlabel('Time (in seconds)')
-ylabel(strcat(y_label_splitted(1,1),' (in',{' '},y_label_splitted(1,2),')'))
-xlim([0 max(time)])
-legend(full_topic,'Interpreter','None')
+plot(time,data);
+title(full_topic,'Interpreter','None');
+xlabel('Time (in seconds)');
+ylabel(strcat(y_label_splitted(1,1),' (in',{' '},y_label_splitted(1,2),')'));
+xlim([0 max(time)]);
+legend(full_topic,'Interpreter','None');
 
 end
